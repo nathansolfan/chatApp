62374932f0deded73a8238f6426b7b0c6d9d1af3
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-04 22:55:12 +0000

    up

--- app/Livewire/Chat/Index.php
+++ app/Livewire/Chat/Index.php
@@ -9,7 +9,7 @@ class Index extends Component
     public function render()
     {
         return view('livewire.chat.index')
-            ->extends('layouts.app');
+        ->layout('layouts.app');
     }
 
 }
