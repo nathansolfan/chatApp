46d770cf9faaee8b1719b028043d09f0defe5ca6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-05 20:59:11 +0000

    upo

--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed  h-full  flex  bg-white  border  lg:shadow-sm overflow-hidden inset-0 lg:top-0-16  lg:inset-x-2 m-auto  lg:h-[90%] rounded-t-lg">
+<div class="fixed  h-full  flex  bg-white  border   lg:shadow-sm overflow-hidden inset-0 lg:top-0-16  lg:inset-x-2 m-auto  lg:h-[90%] rounded-t-lg">
     {{-- Be like water. --}}
     indexxss
     huhhu
