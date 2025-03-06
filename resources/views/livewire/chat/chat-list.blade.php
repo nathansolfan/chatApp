cc38bc526f18e9b01ea764184a709487cfbb1ecf
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-06 19:41:52 +0000

    update chat list and added styling + button

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,4 +1,35 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
-    Chatlist
+<div class="flex flex-col transition-all h-full overflow-hidden">
+
+    <header class="px-3 z-10 bg-white  sticky top-0 w-full py-2  border-2">
+
+        <div class="border-b justify-between flex items-center pb-2">
+            <div class="flex items-center gap-2">
+                <h5 class="font-extrabold text-2xl">Chats</h5>
+
+            </div>
+
+            <button>
+                <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
+                </svg>
+            </button>
+
+        </div>
+
+        {{-- Filters --}}
+
+        <div class="flex gap-3 items-center overflow-x-scroll p-2 bg-white">
+            <button class="inline-flex justify-center items-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border">
+                All
+            </button>
+
+        </div>
+
+    </header>
+
+    <main>
+
+    </main>
 </div>
